ea82e057c5da3116d05e8f1ee63c9b80fe85e7c1
Samuel Rosas <samuel.m.rosas@gmail.com>
2020-10-06 22:54:57 +0300

    Prepare for Heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { json } = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -11,8 +12,10 @@ morgan.token('body', (req, res) => {
   else return " "
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons = [
   {
@@ -101,7 +104,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
